3f3b1e82854d2e4ab852fdd874ed69445a4b570f
B-Shaikshavalli <bshaikshavalli.com>
2024-12-12 22:43:36 +0530

    commit

---
+++ app2/src/com/edubridge/app2/CalcTest.java
@@ -0,0 +1,12 @@
+package com.edubridge.app2;
+
+import com.edubridge.app1.*;
+public class CalcTest {
+
+	public static void main(String[] args) {
+		System.out.println(Calc.findSquare(10));
+		System.out.println(Calc.findCube(10));
+
+	}
+
+}
---
+++ app4/src/com/edubridge/app4/CalcTest.java
@@ -0,0 +1,13 @@
+package com.edubridge.app4;
+
+import com.edubridge.app3.*;
+
+public class CalcTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Calc.findIntrest(200000, 24, 5));
+
+	}
+
+}
